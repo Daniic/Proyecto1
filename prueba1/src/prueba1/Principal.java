b4b265da4de83eaf3109dec810f97321faace3ce
Dani <Dani@PC-DANI>
2021-03-05 10:07:46 +0100

    cambio en cambio1

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,7 +3,8 @@ package prueba1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola world GIT2");
+		System.out.println("Hola world GIT2 Rama nueva");
+		System.out.println("Se añade cambio1");
 	}
 
 }
